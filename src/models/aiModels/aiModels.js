cf89e790a3c8131b4f49a8cb54031e8c54017fa8
maxkonrad <furkanselek@outlook.com>
2023-08-19 03:10:32 +0300

    init ai models

---
+++ src/models/aiModels/aiModels.js
@@ -0,0 +1,24 @@
+const aiModels = {
+    gpt3: {
+        body: {
+            model: 'text-davinci-003',
+            prompt: 'The following is a question from a USER. As GPT, your goal is to provide the very best and the cleverest answer.\n\n',
+            max_tokens: 2000, 
+            temperature: 0.5,
+            stream: false,
+            stop: ['USER:', 'GPT:']
+        }
+    },
+    gpt4: {
+        body: {
+            model: 'gpt-3.5-turbo',
+            prompt: 'The following is a question from a USER. As GPT, your goal is to provide the very best and the cleverest answer.\n\n',
+            max_tokens: 2000, 
+            temperature: 0.5,
+            stream: false,
+            stop: ['USER:', 'GPT:']
+        }
+    },
+}
+
+module.exports = {aiModels}
