6139251cf6261159023786e580be729f0115c534
maxkonrad <furkanselek@outlook.com>
2023-08-19 03:12:09 +0300

    add command registeration script
    This script registers commands for a specific server

---
+++ src/utilities/CommandsManager.js
@@ -0,0 +1,24 @@
+const { REST, Routes, Collection } = require('discord.js');
+const {token, clientId, svId } = require('../config.json')
+
+async function DeployCommands() {
+    const commands = [
+        {
+            name: 'furka',
+            description: 'selam furka'
+        }
+    ]
+    const rest = new REST({ version: '10' }).setToken(token);
+    try {
+        const data = await rest.put(
+            Routes.applicationGuildCommands(clientId, svId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = { DeployCommands }
