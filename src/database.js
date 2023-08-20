643d90ee5c66dece40ed012ef8801d1d922be17c
maxkonrad <furkanselek@outlook.com>
2023-08-20 11:11:18 +0300

    remove unnecessary file

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const {mongoose} = require('mongoose')
-const {mongoUri} = require('../config.json')
-
-
-mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true})
-    
